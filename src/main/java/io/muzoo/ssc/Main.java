999e1f84595d1af29906a813ad5a413daa1acd8e
Sungchunn <99805001+Sungchunn@users.noreply.github.com>
2025-01-27 00:20:48 +0700

    first commit: setup project

---
+++ src/main/java/io/muzoo/ssc/Main.java
@@ -0,0 +1,10 @@
+package io.muzoo.ssc;
+
+import io.muzoo.ssc.assignment.tracker.SscAssignment;
+
+public class Main extends SscAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
